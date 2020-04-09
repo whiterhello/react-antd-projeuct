ed3b2b801a80492c3dea9360e1e97a54cc81bea4
Lijl <18443850+whiterhello@users.noreply.github.com>
2020-04-09 16:39:37 +0800

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class AdminLayout extends Component {
+    state = {  }
+    render() { 
+        return ( 
+          <div></div>
+        );
+    }
+}
+ 
+export default AdminLayout;
