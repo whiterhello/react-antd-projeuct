a21b3fb5e111ee6078bcb7dbe0483f716dcf5a06
li <1406307492@qq.com>
2020-04-27 10:55:00 +0800

    数据分析页面

---
+++ src/page/dataAnalysis/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Layout } from 'antd';
+import './style/index.less'
+
+class DataAnalysis extends Component {
+    render() {
+        return (
+            <div>
+                数据分析
+            </div>
+        );
+    }
+}
+
+export default DataAnalysis;
